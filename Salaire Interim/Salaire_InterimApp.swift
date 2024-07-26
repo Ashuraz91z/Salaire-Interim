be04aeb2d2a02c0515095acfb0f17e5df686f31a
Lucas Fernandes <lucas.epitech@epitech.digital>
2024-07-26 07:28:24 +0200

    Initial Commit

---
+++ Salaire Interim/Salaire_InterimApp.swift
@@ -0,0 +1,17 @@
+//
+//  Salaire_InterimApp.swift
+//  Salaire Interim
+//
+//  Created by Lucas Fernandes on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct Salaire_InterimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
